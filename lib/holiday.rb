713b9bc1cb833450cee811d7d63e2d187919a170
Nancy Jackson <nbjackson23@gmail.com>
2020-08-09 16:33:03 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,18 +51,20 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
-
+      end
+    end
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   result = holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
-        end
-      end
+      
+      
     end
   end
-binding.pry
+result.flatten.compact
 end
 
 
