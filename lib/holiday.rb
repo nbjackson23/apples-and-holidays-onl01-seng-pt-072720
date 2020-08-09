978385ea1c949fbab6003567b775aea7eb342523
Nancy Jackson <nbjackson23@gmail.com>
2020-08-09 16:26:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
-
+      end
+    end
 end
 
 
