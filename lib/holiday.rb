076d5eef2cb34c27e6398829576edcb48c6c06bc
Nancy Jackson <nbjackson23@gmail.com>
2020-08-09 16:27:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,6 +56,16 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 
+def all_holidays_with_bbq(holiday_hash)
+  result = holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+      
+      
+    end
+  end
+binding.pry
+end
 
 
 
